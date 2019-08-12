2a07d9f8d5875701c86c31a51ebf3d6399f40b07
shofyan <pencari.angin1@gmail.com>
2019-08-12 19:54:40 +0700

    goroutine__channel

--- 18_goroutine_channel/main.go
+++ 18_goroutine_channel/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 
+	done := make(chan bool)
+
+	go helloGo(done)
+	<-done
+
+	fmt.Println("hai semua")
+}
+
+func helloGo(done chan bool) {
+	fmt.Println("hello semua")
+	done <- true
 }
