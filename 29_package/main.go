fceed6ff7b8d0ba2111f1369aa357c794eeca722
Shofyan <Shofyan@terminix.co.id>
2019-08-13 15:03:14 +0700

    ganti refference di go

--- 29_package/main.go
+++ 29_package/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 
-	m1 "github.com/shofyan/LearnGolang/29_package/config"
-	"github.com/shofyan/LearnGolang/29_package/model"
+	"./config"
+	"./model"
 )
 
 func main() {
 
-	pc := m1.GetPostgresConnection()
+	pc := config.GetPostgresConnection()
 	fmt.Println(pc)
 
 	pp := model.Person{ID: 1, Name: "shofyan"}
