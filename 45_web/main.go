c827cea60bf6b1eba2a132394bf2c848967936ed
shofyan <pencari.angin1@gmail.com>
2019-08-18 07:51:52 +0700

    web

--- 45_web/main.go
+++ 45_web/main.go
@@ -1,10 +1,30 @@
+// https://dasarpemrogramangolang.novalagung.com/48-web.html
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+)
 import "net/http"
 
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "hai dunia apakabar")
+}
+
+func displayMessage(w http.ResponseWriter, r *http.Request) {
+	var data = map[string]string{
+		"Name":    "Shofyan",
+		"Message": "Apa kabar dunia, selamat pagi",
+	}
+
+	var t, err = template.ParseFiles("template.html")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	t.Execute(w, data)
+
 }
 
 func main() {
@@ -12,7 +32,8 @@ func main() {
 		fmt.Fprintln(writer, "assalamualaikum ")
 	})
 	http.HandleFunc("/index", index)
+	http.HandleFunc("/display", displayMessage)
 
-	fmt.Println("starting webserver at http://localhost:8080")
+	fmt.Println("starting webserver at http://localhost:8181")
 	http.ListenAndServe(":8181", nil)
 }
