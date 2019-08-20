9f2dbd2606e7a92f95a45dfbb601e29e02aa6edf
Shofyan <pencari.angin1@gmail.com>
2019-08-20 16:48:30 +0700

    parsial html

--- 57_routing_static_asset/main.go
+++ 57_routing_static_asset/main.go
@@ -2,11 +2,33 @@ package main
 
 import "fmt"
 import "net/http"
+import "html/template"
+import "path"
 
 func main() {
 	http.Handle("/static/",
 		http.StripPrefix("/static/",
 			http.FileServer(http.Dir("assets"))))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		var filePath = path.Join("views", "index.html")
+		var tmpl, err = template.ParseFiles(filePath)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		data := map[string]interface{}{
+			"title": "Learning golang web",
+			"name":  "rayden",
+		}
+
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
 
 	fmt.Println("server started at localhost:9000")
 	http.ListenAndServe(":9000", nil)
