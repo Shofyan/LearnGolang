658b6a8f7a861abbb3ae94c8e7b5b10144fef8e3
Shofyan <pencari.angin1@gmail.com>
2019-08-21 18:53:56 +0700

    form value

--- 65_form_value/main.go
+++ 65_form_value/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func routeIndexGet(w http.ResponseWriter, r *http.Request) {
@@ -19,10 +22,39 @@ func routeIndexGet(w http.ResponseWriter, r *http.Request) {
 
 func routeSubmitPost(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
-		var tmpl = template.Must(template.New("result").ParseFiles("view.html"))
-		if err := r.ParseForm(); err != nil {
+		var tmpl = template.Must(template.New("form").ParseFiles("view.html"))
+		if err := r.ParseMultipartForm(1024); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
+
+		alias := r.FormValue("alias")
+		uploadedFile, handler, err := r.FormFile("file")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer uploadedFile.Close()
+		dir, err := os.Getwd()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		fileName := handler.Filename
+		if alias != "" {
+			fileName = fmt.Sprintf("%s%s", alias, filepath.Ext(handler.Filename))
+		}
+
+		fileLocation := filepath.Join(dir, "files", fileName)
+		targetFIle, err := os.OpenFile(fileLocation, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		defer targetFIle.Close()
+
+		if _, err := io.Copy(targetFIle, uploadedFile); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write([]byte("done"))
 
 		var name = r.FormValue("name")
 		var message = r.FormValue("message")
