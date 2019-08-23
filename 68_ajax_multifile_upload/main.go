39ecb530e968ff5558521388b450e2182fbd548e
Shofyan <pencari.angin1@gmail.com>
2019-08-23 10:16:38 +0700

    ajax multiple file

--- 68_ajax_multifile_upload/main.go
+++ 68_ajax_multifile_upload/main.go
@@ -1,44 +1,42 @@
 package main
 
-import (
-	"fmt"
-	"html/template"
-	"io"
-	"net/http"
-	"os"
-	"path/filepath"
-)
+import "fmt"
+import "net/http"
+import "html/template"
+import "path/filepath"
+import "io"
+import "os"
 
 func main() {
 	http.HandleFunc("/", handleIndex)
 	http.HandleFunc("/upload", handleUpload)
-	http.Handle("/static/",
-		http.StripPrefix("/static/",
-			http.FileServer(http.Dir("assets"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("assets"))))
 
-	fmt.Println("sertver started at locahost:9900")
+	fmt.Println("server started at localhost:9000")
 	http.ListenAndServe(":9000", nil)
-
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("view.html"))
 	if err := tmpl.Execute(w, nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
 	}
 }
 
 func handleUpload(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
-		http.Error(w, "only accept post request", http.StatusBadRequest)
+		http.Error(w, "Only accept POST request", http.StatusBadRequest)
 		return
 	}
+
 	basePath, _ := os.Getwd()
+
 	reader, err := r.MultipartReader()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+
 	for {
 		part, err := reader.NextPart()
 		if err == io.EOF {
@@ -50,16 +48,16 @@ func handleUpload(w http.ResponseWriter, r *http.Request) {
 		if dst != nil {
 			defer dst.Close()
 		}
-
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 
 		if _, err := io.Copy(dst, part); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
-
 	}
-	w.Write([]byte("all file uploaded"))
 
+	w.Write([]byte(`all files uploaded`))
 }
