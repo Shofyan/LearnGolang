e21cba3f9ed5be14076135817af33de5cb715503
Shofyan <pencari.angin1@gmail.com>
2019-08-23 14:21:31 +0700

    HTTP basic middle ware

---
+++ 70_http_basic_auth/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/student", ActionStudent)
+
+	fmt.Println("server started at localhost:9000")
+	http.ListenAndServe(":9000", nil)
+}
+
+func ActionStudent(w http.ResponseWriter, r *http.Request) {
+	if !Auth(w, r) {
+		return
+	}
+	if !AllowOnlyGet(w, r) {
+		return
+	}
+
+	if id := r.URL.Query().Get("id"); id != "" {
+		OutputJSON(w, SelectStudent(id))
+		return
+	}
+}
+
+func OutputJSON(w http.ResponseWriter, o interface{}) {
+	res, err := json.Marshal(o)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}
