86208193b1a09ca0fc437419b08f50327514aa95
Shofyan <pencari.angin1@gmail.com>
2019-08-27 08:37:40 +0700

    logrus

--- 80_logrus_and_echo_middleware/main.go
+++ 80_logrus_and_echo_middleware/main.go
@@ -4,8 +4,44 @@ import (
 	"fmt"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	log "github.com/sirupsen/logrus"
 	"net/http"
+	"time"
 )
+
+func MakeLogEntry(c echo.Context) *log.Entry {
+	if c == nil {
+		return log.WithFields(log.Fields{
+			"at": time.Now().Format("2006-01-02 15:04:05"),
+		})
+	}
+
+	return log.WithFields(log.Fields{
+		"at":     time.Now().Format("2006-01-02 15:04:05"),
+		"method": c.Request().Method,
+		"uri":    c.Request().URL.String(),
+		"ip":     c.Request().RemoteAddr,
+	})
+}
+
+func MiddlewareLogging(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		MakeLogEntry(c).Info("incoming request")
+		return next(c)
+	}
+}
+
+func ErrorHandler(err error, c echo.Context) {
+	report, ok := err.(*echo.HTTPError)
+	if ok {
+		report.Message = fmt.Sprintf("http error %d - %v", report.Code, report.Message)
+	} else {
+		report.Message = echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	MakeLogEntry(c).Error(report.Message)
+	c.HTML(report.Code, report.Message.(string))
+}
 
 func main() {
 	e := echo.New()
@@ -14,6 +50,10 @@ func main() {
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "method=${method}, uri=${uri}, status=${status}\n",
 	}))
+
+	e.Use(MiddlewareLogging)
+	e.HTTPErrorHandler = ErrorHandler
+
 	e.Use(MiddlewareOne)
 	e.Use(MiddlewareTwo)
 	e.Use(echo.WrapMiddleware(MiddlewareSomething))
@@ -23,7 +63,19 @@ func main() {
 		return context.JSON(http.StatusOK, true)
 	})
 
-	e.Logger.Fatal(e.Start(":9000"))
+	//e.Logger.Fatal(e.Start(":9000"))
+
+	lock := make(chan error)
+	go func(lock chan error) { lock <- e.Start(":9000") }(lock)
+
+	time.Sleep(1 * time.Second)
+	MakeLogEntry(nil).Warning("Application start with ssl/tls enable ")
+
+	err := <-lock
+	if err != nil {
+		MakeLogEntry(nil).Panic("failed start application")
+	}
+
 }
 
 func MiddlewareOne(next echo.HandlerFunc) echo.HandlerFunc {
